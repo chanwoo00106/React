8f3dbfbef3743949882026541ceab5e359a36aa9
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-07-29 19:10:42 +0900

    react-virtualized 로 최적화
    사실 하나도 이해 못 했음

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { List } from 'react-virtualized'
 import TodoListItem from './TodoListItem'
 import './TodoList.scss'
 
-export default function TodoList({todos, onRemove, onToggle}) {
+export default React.memo(function TodoList({todos, onRemove, onToggle}) {
+    const rowRenderer = useCallback(({index, key, style}) => {
+        const todo = todos[index];
+        return (
+            <TodoListItem
+            todo={todo}
+            key={key}
+            onRemove={onRemove}
+            onToggle={onToggle}
+            style={style} />
+        );
+    }, [onRemove, onToggle, todos]);
+
     return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-            ))}
-        </div>
+        <List
+        className="TodoList"
+        width={512}
+        height={513}
+        rowCount={todos.length}
+        rowHeight={57}
+        rowRenderer={rowRenderer}
+        list={todos}
+        style={{outline: 'none'}} />
     )
-}
+});
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -3,18 +3,20 @@ import {MdCheckBoxOutlineBlank, MdRemoveCircleOutline, MdCheckBox} from 'react-i
 import cn from 'classnames'
 import './TodoListItem.scss'
 
-function TodoListItem({todo, onRemove, onToggle}) {
+function TodoListItem({todo, onRemove, onToggle, style}) {
 
     const {id, text, checked} = todo;
 
     return (
-        <div className="TodoListItem">
-            <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove" onClick={() => onRemove(id)}>
-                <MdRemoveCircleOutline />
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className="text">{text}</div>
+                </div>
+                <div className="remove" onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline />
+                </div>
             </div>
         </div>
     )
