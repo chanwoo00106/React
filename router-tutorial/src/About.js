3aad672bac85325d9b0bcaacefc253338938bfc2
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-01 22:45:48 +0900

    Update Aboute
    URL 쿼리 추가

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import qs from 'qs';
 
-function About() {
+function About({ location }) {
+    console.log(location)
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true // 이 설정으로 문자열 맨 앞에 ?를 생략합니다.
+    });
+    console.log(query)
+    const showDetail = query.detail === 'true';
     return (
         <div>
             <h1>소개</h1>
             <p>리액트 라우터 기초 공부</p>
+            {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
         </div>
     )
 }
