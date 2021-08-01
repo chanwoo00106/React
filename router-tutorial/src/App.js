0fe56ec0f9257d046fa70e86083920fb6937fb1d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-01 21:26:21 +0900

    Update router-tutorial > App.js
    Link 컴포넌트를 사용해 다른 주소로 이동

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Example from './Example';
@@ -7,6 +7,17 @@ import Example from './Example';
 function App() {
   return (
     <div className="App">
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/example">test</Link>
+        </li>
+      </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
       <Route path="/example" component={Example} />
