e115711cebd39908062dc2a4bef1a7348f636119
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-05 15:36:30 +0900

    add NewsItem.js, NewsList.js
    news-viewer 준비

--- news-viwer/src/App.js
+++ news-viwer/src/App.js
@@ -1,27 +1,9 @@
-import React, {useState} from 'react';
 import './App.css';
-import axios from 'axios';
+import NewsList from './components/NewsList';
+
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      setData(res.data);
-    }catch(e) {
-      console.log(e);
-    }
-  }
-  return (
-    <div className="App">
-      <div>
-        <div>
-          <button onClick={onClick}>불러오기</button>
-        </div>
-        {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-      </div>
-    </div>
-  );
+  return <NewsList />
 }
 
 export default App;
