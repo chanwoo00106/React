7b2cd9b2dcbe607f69d740257d6c61210b7dffd1
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-05 15:50:46 +0900

    Update NewsList.js
    api 적용

--- news-viwer/src/components/NewsList.js
+++ news-viwer/src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -15,22 +16,36 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160'
-};
-
 function NewsList() {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const res = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=f9ceb37eab5e42fe8853946fe69d3cca');
+                setArticles(res.data.articles);
+            } catch(e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>
+    }
+    if (!articles) {
+        return null;
+    }
+
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map(article => (
+                <NewsItem key={article.url} article={article} />
+            ))}
         </NewsListBlock>
     )
 }
