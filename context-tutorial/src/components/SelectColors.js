af175e3ef72bd44abb602f5eb3b7a969380a8f57
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-10 16:11:12 +0900

    Update SelectColors.js
    동적 context 사용

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ColorConsumer } from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
@@ -6,19 +7,28 @@ const SelectColors = () => {
     return (
         <div>
             <h2>색상을 선택하세요.</h2>
-            <div style={{display: 'flex'}}>
-                {colors.map(color => (
-                    <div
-                    key={color}
-                    style={{
-                        background: color,
-                        width: '24px',
-                        height: '24px',
-                        cursor: 'pointer'
-                    }}
-                    />
-                ))}
-            </div>
+                <ColorConsumer>
+                    {({actions}) =>
+                        <div style={{display: 'flex'}}>
+                            {colors.map(color => (
+                                    <div
+                                    key={color}
+                                    style={{
+                                        background: color,
+                                        width: '24px',
+                                        height: '24px',
+                                        cursor: 'pointer'
+                                    }}
+                                    onClick={() => actions.setColor(color)}
+                                    onContextMenu={e => {
+                                        e.preventDefault();// 마우스 오른쪽 클릭 시 메뉴가 뜨는 것을 무시함
+                                        actions.setSubcolor(color);
+                                    }}
+                                />
+                            ))}
+                        </div>
+                    }
+                </ColorConsumer>
             <hr />
         </div>
     );
