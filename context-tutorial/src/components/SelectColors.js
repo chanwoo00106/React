f3fc74aaa679030faeef578a03a1e74d136c7dee
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-11 16:26:37 +0900

    Update SelectColors.js
    static contextType을 하려 했는데 안되네

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import { ColorConsumer } from '../contexts/color';
+import React, {Component} from 'react';
+import ColorContext from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const SelectColors = () => {
-    return (
-        <div>
-            <h2>색상을 선택하세요.</h2>
-            <ColorConsumer>
-                {({actions}) =>
-                    <div style={{display: 'flex'}}>
-                        {colors.map(color => (
-                            <div
-                                key={color}
-                                style={{
-                                    background: color,
-                                    width: '24px',
-                                    height: '24px',
-                                    cursor: 'pointer'
-                                }}
-                                onClick={() => actions.setColor(color)}
-                                onContextMenu={e => {
-                                    e.preventDefault();// 마우스 오른쪽 클릭 시 메뉴가 뜨는 것을 무시함
-                                    actions.setSubcolor(color);
-                                }}
-                            />
-                        ))}
-                    </div>
-                }
-            </ColorConsumer>
-            <hr />
-        </div>
-    );
+class SelectColors extends Component {
+    static contextType = ColorContext;
+
+    handleSetColor = color => {
+        this.context.action.setColor(color);
+    }
+
+    handleSetSubcolor = subcolor => {
+        this.context.action.setSubcolor(subcolor);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>색상을 선택하세요.</h2>
+                <div style={{display: "flex"}}>
+                    {colors.map(color => (
+                        <div
+                            key={color}
+                            style={{
+                                background: color,
+                                width: "24px",
+                                height: "24px",
+                                cursor: "pointer"
+                            }}
+                            onClick={() => this.handleSetColor(color)}
+                            onContextMenu={e => {
+                                e.preventDefault();
+                                this.handleSetSubcolor(color);
+                            }}
+                        />
+                    ))}
+                </div>
+                <hr />
+            </div>
+        );
+    };
 };
 
 export default SelectColors;
