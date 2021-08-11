0c4c04cc11fe9276daf25c6b7a398498938a9f83
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-11 15:18:48 +0900

    Update ColorBox.js
    Consumer 대신 Hook 사용

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -7,28 +7,28 @@ const SelectColors = () => {
     return (
         <div>
             <h2>색상을 선택하세요.</h2>
-                <ColorConsumer>
-                    {({actions}) =>
-                        <div style={{display: 'flex'}}>
-                            {colors.map(color => (
-                                    <div
-                                    key={color}
-                                    style={{
-                                        background: color,
-                                        width: '24px',
-                                        height: '24px',
-                                        cursor: 'pointer'
-                                    }}
-                                    onClick={() => actions.setColor(color)}
-                                    onContextMenu={e => {
-                                        e.preventDefault();// 마우스 오른쪽 클릭 시 메뉴가 뜨는 것을 무시함
-                                        actions.setSubcolor(color);
-                                    }}
-                                />
-                            ))}
-                        </div>
-                    }
-                </ColorConsumer>
+            <ColorConsumer>
+                {({actions}) =>
+                    <div style={{display: 'flex'}}>
+                        {colors.map(color => (
+                            <div
+                                key={color}
+                                style={{
+                                    background: color,
+                                    width: '24px',
+                                    height: '24px',
+                                    cursor: 'pointer'
+                                }}
+                                onClick={() => actions.setColor(color)}
+                                onContextMenu={e => {
+                                    e.preventDefault();// 마우스 오른쪽 클릭 시 메뉴가 뜨는 것을 무시함
+                                    actions.setSubcolor(color);
+                                }}
+                            />
+                        ))}
+                    </div>
+                }
+            </ColorConsumer>
             <hr />
         </div>
     );
