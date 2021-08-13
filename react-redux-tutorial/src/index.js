6f0bd6af9434efb2d0ef201fcd4c89fa2dec5e51
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-13 22:24:10 +0900

    Add redux-devtools-extension

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
