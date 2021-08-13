f2270e645e7024515b8cc76a79811b9be8a19773
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-13 17:43:00 +0900

    Add Counter.js
    카운터 컴포넌트 생성

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      
+      <Counter number={0} />
     </div>
   );
 }
