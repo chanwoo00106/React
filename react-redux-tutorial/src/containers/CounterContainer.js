fa3beafc3410462f9707783eb2b9248219d50810
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-15 21:55:20 +0900

    Update CounterContainer.js
    설명 추가

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -4,7 +4,6 @@ import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
-    console.log(increase, decrease);
     return (
         <div>
             <Counter number={number} onIncrease={increase} onDecrease={decrease} />
@@ -12,12 +11,26 @@ const CounterContainer = ({ number, increase, decrease }) => {
     )
 };
 
+// mapStateToProps 는 리덕스 스토어의 상태를 조회해서 어떤 것들을 props 로 넣어줄지 정의합니다.
+// 현재 리덕스 상태를 파라미터로 받아옵니다.
+const mapStateToProps = state => ({
+    number: state.counter.number,
+});
+
+// mapDispatchToProps가 함수가 아니라 객체면
+// bindActionCreators 를 connect 에서 대신 해줍니다.
+const mapDispatchToProps = dispatch => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease())
+});
+
+// connect 함수에는 mapStateToProps, mapDispatchToProps 를 인자로 넣어주세요.
 export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {
-        increase,
-        decrease
-    }
+    mapStateToProps,
+    mapDispatchToProps
 )(CounterContainer);
+
+/* 위 코드는 다음과 동일합니다.
+    const enhance = connect(mapStateToProps, mapDispatchToProps);
+    export defualt enhance(CounterContainer);
+*/
