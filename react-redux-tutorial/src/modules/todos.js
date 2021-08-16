449678080655b49c3d4ce11e673f8a7d41d5866c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-16 18:28:35 +0900

    Update todos.js
    더 깔끔하게

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -61,20 +61,20 @@ const initialState = {
 
 const todos = handleActions(
     {
-        [CHANGE_INPUT]: (state, action) => ({...state, input: action.payload}),
-        [INSERT]: (state, action) => ({
+        [CHANGE_INPUT]: (state, {payload: input}) => ({...state, input}),
+        [INSERT]: (state, {payload: todo}) => ({
             ...state,
-            todos: state.todos.concat(action.payload)
+            todos: state.todos.concat(todo)
         }),
-        [TOGGLE]: (state, action) => ({
+        [TOGGLE]: (state, {payload: id}) => ({
             ...state,
             todos: state.todos.map(todo => 
-                todo.id === action.payload ? {...todo, done: !todo.done}: todo
+                todo.id === id ? {...todo, done: !todo.done}: todo
             )
         }),
-        [REMOVE]: (state, action) => ({
+        [REMOVE]: (state, {payload: id}) => ({
             ...state,
-            todos: state.todos.filter(todo => todo.id !== action.payload)
+            todos: state.todos.filter(todo => todo.id !== id)
         })
     },
     initialState
