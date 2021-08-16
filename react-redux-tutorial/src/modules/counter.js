dd6d8f0e8d23f8a5247f11d2b8f82bfe9fa464e7
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-16 17:56:33 +0900

    use redux-actions

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,26 +1,45 @@
+import { createAction, handleActions } from 'redux-actions';
+
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-export const increase = () => ({type: INCREASE});
-export const decrease = () => ({type: DECREASE});
+// 더 쉽게 액션 함수를 선언할 수 있다.
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+// 위 코드는 아래와 같음
+// export const increase = () => ({type: INCREASE});
+// export const decrease = () => ({type: DECREASE});
+
 
 const initialState = {
     number: 0
 }
 
-function counter(state=initialState, action){
-    switch(action.type) {
-        case INCREASE:
-            return {
-                number: state.number + 1
-            };
-        case DECREASE:
-            return {
-                number: state.number - 1
-            };
-        default:
-            return state;
-    }
-}
+// 첫 번째 파라미터에는 각 액션에 대한 업데이트 함수를 넣어 주고,
+// 두 번째 파라미터에는 초기 상태를 넣어 줌
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({number: state.number + 1}),
+        [DECREASE]: (state, action) => ({number: state.number - 1}),
+    },
+    initialState
+)
+
+// 위 코드는 아래와 같다
+// function counter(state=initialState, action){
+//     switch(action.type) {
+//         case INCREASE:
+//             return {
+//                 number: state.number + 1
+//             };
+//         case DECREASE:
+//             return {
+//                 number: state.number - 1
+//             };
+//         default:
+//             return state;
+//     }
+// }
 
 export default counter;
