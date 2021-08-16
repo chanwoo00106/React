573c1e51d4bd69cc3ba57d72f5345fdd90183b3d
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-16 18:39:44 +0900

    use immer
    불변성을 쉽게 유지하기

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
 
 const CHANGE_INPUT = 'CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
@@ -61,21 +62,28 @@ const initialState = {
 
 const todos = handleActions(
     {
-        [CHANGE_INPUT]: (state, {payload: input}) => ({...state, input}),
-        [INSERT]: (state, {payload: todo}) => ({
-            ...state,
-            todos: state.todos.concat(todo)
-        }),
-        [TOGGLE]: (state, {payload: id}) => ({
-            ...state,
-            todos: state.todos.map(todo => 
-                todo.id === id ? {...todo, done: !todo.done}: todo
-            )
-        }),
-        [REMOVE]: (state, {payload: id}) => ({
-            ...state,
-            todos: state.todos.filter(todo => todo.id !== id)
-        })
+        [CHANGE_INPUT]: (state, {payload: input}) => (
+            produce(state, draft => {
+                draft.input = input;
+            })
+        ),
+        [INSERT]: (state, {payload: todo}) => (
+            produce(state, draft => {
+                draft.todos.push(todo);
+            })
+        ),
+        [TOGGLE]: (state, {payload: id}) => (
+            produce(state, draft => {
+                const todo = draft.todos.find(todo => todo.id === id);
+                todo.done = !todo.done;
+            })
+        ),
+        [REMOVE]: (state, {payload: id}) => (
+            produce(state, draft => {
+                const index = draft.todos.findIndex(todo => todo.id === id);
+                draft.todos.splice(index, 1);
+            })
+        )
     },
     initialState
 );
