4dcce8c318c26d5288e15e7eec7969ad6dfa341b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-17 15:58:33 +0900

    Update CounterContainer.js
    Hooks로 컨테이너 컴포넌트 만들기

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
     return (
-        <div>
-            <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-        </div>
-    )
+        <Counter
+            number={number}
+            onIncrease={() => dispatch(increase())}
+            onDecrease={() => dispatch(decrease())}
+        />
+    );
 };
 
-// mapStateToProps 는 리덕스 스토어의 상태를 조회해서 어떤 것들을 props 로 넣어줄지 정의합니다.
-// 현재 리덕스 상태를 파라미터로 받아옵니다.
-const mapStateToProps = state => ({
-    number: state.counter.number,
-});
-
-// mapDispatchToProps가 함수가 아니라 객체면
-// bindActionCreators 를 connect 에서 대신 해줍니다.
-const mapDispatchToProps = dispatch => ({
-    increase: () => dispatch(increase()),
-    decrease: () => dispatch(decrease())
-});
-
-// connect 함수에는 mapStateToProps, mapDispatchToProps 를 인자로 넣어주세요.
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(CounterContainer);
-
-/* 위 코드는 다음과 동일합니다.
-    const enhance = connect(mapStateToProps, mapDispatchToProps);
-    export defualt enhance(CounterContainer);
-*/
+export default CounterContainer;
