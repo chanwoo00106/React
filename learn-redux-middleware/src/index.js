ec947df353fc83448e4d0c3ee7b8723af94bc37a
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-17 23:05:06 +0900

    Update index.js
    redux 적용

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './App';
+import rootReducer from './modules'; // index는 생략 가능
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
