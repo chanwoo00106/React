5889db093ea9b11df327eef1185e8ff47dd39da1
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-18 15:41:29 +0900

    Create loggerMiddleware.js
    미들웨어 생성

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import App from './App';
 import rootReducer from './modules'; // index는 생략 가능
+import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
