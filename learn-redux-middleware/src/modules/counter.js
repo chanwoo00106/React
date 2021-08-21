0c9929aad5a26bdcc4f660eb219de132c86b49cd
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-21 21:14:06 +0900

    유용한 기능 추가

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,5 +1,5 @@
 import {createAction, handleActions} from 'redux-actions';
-import {delay, put, takeEvery, takeLatest} from 'redux-saga/effects'
+import {delay, put, takeLatest, select, throttle} from 'redux-saga/effects'
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -18,6 +18,8 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga() {
     yield delay(1000); // 1초를 기다립니다.
     yield put(increase()); // 특정 액션을 디스패치합니다.
+    const number = yield select(state => state.counter);
+    console.log(`현재 값은 ${number}입니다.`);
 }
 
 function* decreaseSaga() {
@@ -27,7 +29,7 @@ function* decreaseSaga() {
 
 export function* counterSaga() {
     // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해 줍니다.
-    yield takeEvery(INCREASE_ASYNC, increaseSaga);
+    yield throttle(3000, INCREASE_ASYNC, increaseSaga);
     // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고
     // 가장 마지막에 실행된 작업만 수행합니다.
     yield takeLatest(DECREASE_ASYNC, decreaseSaga);
