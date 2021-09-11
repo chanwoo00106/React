abc9667e7fb25115d1db17f1f44495fbc068aa21
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-11 23:09:25 +0900

    action과 contact.js 생성

---
+++ practice-redux/src/state/action-types/index.js
@@ -0,0 +1,5 @@
+export const ActionType = {
+    ADD: 'add',
+    DLETE: 'delete',
+    EDIT: 'edit'
+}
---
+++ practice-redux/src/state/reducers/contact.js
@@ -0,0 +1,35 @@
+import { ActionType } from "../action-types";
+
+const initialState = [{
+    id: 1,
+    name: 'Teemo',
+    email: 'example@example.com',
+    phone: '010-1234-1234'
+}]
+
+const reducer = (state=initialState, action) => {
+    switch(action.type){
+        case ActionType.ADD:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    name: action.name,
+                    email: action.email,
+                    phone: action.phone
+                }
+            ];
+        case ActionType.DLETE:
+            return state.filter(x => x !== action.id);
+        case ActionType.EDIT:
+            return state[action.id - 1] = {
+                id: action.id,
+                name: action.name,
+                email: action.email,
+                phone: action.phone
+            }
+        default: return state;
+    }
+}
+
+export default reducer;
