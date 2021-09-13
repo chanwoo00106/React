ae7eb8608bd4eacdd4e3a845862eb0bc9cdb8347
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-13 13:25:30 +0900

    파라미터와 쿼리

--- blog/blog-backend/index.js
+++ blog/blog-backend/index.js
@@ -7,6 +7,11 @@ router.get('/', ctx => {
     ctx.body = '홈';
 });
 
+router.get('/about/:name?', ctx => {
+    const {name} = ctx.params;
+    ctx.body = name ? `${name}의 소개` : '포스트가 없습니다.';
+});
+
 router.get('/about', ctx => {
     ctx.body = '소개';
 });
