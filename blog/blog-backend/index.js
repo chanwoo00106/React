da322b870b9794aabefacb686c327b807129e993
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-13 13:06:47 +0900

    query

--- blog/blog-backend/index.js
+++ blog/blog-backend/index.js
@@ -4,6 +4,11 @@ const app = new Koa();
 app.use((ctx, next) => {
     console.log(ctx.url);
     console.log(1);
+    console.log(ctx.query.authorized)
+    if (ctx.query.authorized !== '1') {
+        ctx.status = 401;
+        return;
+    }
     next();
 });
 app.use((ctx, next) => {
