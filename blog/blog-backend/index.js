0102503ffbe0b802ba46c0cb92d8b2d48321c6bc
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-13 12:39:52 +0900

    learned next

--- blog/blog-backend/index.js
+++ blog/blog-backend/index.js
@@ -1,8 +1,18 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(ctx => {
-    ctx.body = 'hello world koa';
+app.use((ctx, next) => {
+    console.log(ctx.url);
+    console.log(1);
+    next();
+});
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+});
+
+app.use((ctx) => {
+    ctx.body = 'hello world'
 });
 
 app.listen(4000, () => {
