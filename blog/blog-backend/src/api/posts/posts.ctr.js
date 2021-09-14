d548a0e387a09c045d1ec2d628c586da5920a070
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-14 20:56:23 +0900

    get list 생성

--- blog/blog-backend/src/api/posts/posts.ctr.js
+++ blog/blog-backend/src/api/posts/posts.ctr.js
@@ -40,7 +40,22 @@ export const write = async ctx => {
     }
 };
 
-export const list = ctx => {};
+export const list = async ctx => {
+    const page = parseInt(ctx.query.page || '1', 10);
+
+    if (page < 1) {
+        ctx.status = 400;
+        return;
+    }
+    try {
+        const posts = await Post.find().sort({_id: -1}).limit(10).skip((page - 1) * 10).exec();
+        const postCount = await Post.countDocuments().exec();
+        ctx.set('Last-Page', Math.ceil(postCount / 10));
+        ctx.body = posts;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 
 export const read = async ctx => {
     const { id } = ctx.params;
