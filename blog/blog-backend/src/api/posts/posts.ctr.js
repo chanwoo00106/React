d6e1a04353b24c1036f78db0cea9b6fc8e24fd04
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-14 20:03:32 +0900

    삭제 기능과 수정기능

--- blog/blog-backend/src/api/posts/posts.ctr.js
+++ blog/blog-backend/src/api/posts/posts.ctr.js
@@ -19,7 +19,6 @@ export const list = ctx => {};
 
 export const read = async ctx => {
     const { id } = ctx.params;
-    console.log(id);
     try {
         const post = await Post.findById(id).exec();
         if(!post){
@@ -32,6 +31,28 @@ export const read = async ctx => {
     }
 };
 
-export const remove = ctx => {};
+export const remove = async ctx => {
+    const {id} = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 
-export const update = ctx => {};
+export const update = async ctx => {
+    const {id} = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
