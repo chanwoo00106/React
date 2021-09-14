efcc1c3e498e37be13c8daba05136a44b4b1f186
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-14 23:25:59 +0900

    jwt 토큰 발급하기

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -24,12 +24,47 @@ export const register = async ctx => {
         await user.save();
 
         ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60* 24 * 7, // 7일
+            httpOnly: true
+        })
     } catch (e) {
         ctx.throw(500, e);
     }
 }
 
-export const login = async ctx => {}
+export const login = async ctx => {
+    const {username, password} = ctx.request.body;
+   
+    if (!username||!password) {
+        ctx.status = 401; // Unauthorized
+        return;
+    }
+    try {
+        const user = await User.findByUsername(username);
+        // 계정이 존재하지 않으면 에러 처리
+        if (!user) {
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+        if (!valid) {
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60* 24 * 7, // 7일
+            httpOnly: true
+        })
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+}
 
 export const check = async ctx => {}
 
--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
     username: String,
@@ -24,6 +25,20 @@ UserSchema.methods.serialize = function() {
     const data = this.toJSON();
     delete data.hashedPassword;
     return data;
+}
+
+UserSchema.methods.generateToken = function() {
+    const token = jwt.sign(
+        {
+            _id: this.id,
+            username: this.username
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '7d' // 7일 동안 유효함
+        }
+    )
+    return token;
 }
 
 const User = mongoose.model('User', UserSchema);
