b66a3ac6784dc0b6abd2943caa18d269c725c6dd
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-14 09:35:44 +0900

    esm 사용

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const api = require('./api');
+const api = require('../api');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const router = new Router();
