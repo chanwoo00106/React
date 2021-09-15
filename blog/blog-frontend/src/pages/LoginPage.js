f402223cd73483b7e3fe2d914a1abd1cfa35eb3c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-15 22:46:38 +0900

    AuthTemplate 완성

--- blog/blog-frontend/src/pages/LoginPage.js
+++ blog/blog-frontend/src/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
 
 const LoginPage = () => {
     return (
-        <div>
-            로그인
-        </div>
+        <AuthTemplate>
+            <AuthForm />
+        </AuthTemplate>
     )
 }
 
--- blog/blog-frontend/src/pages/RegisterPage.js
+++ blog/blog-frontend/src/pages/RegisterPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
 
 const RegisterPage = () => {
     return (
-        <div>
-            회원가입
-        </div>
+        <AuthTemplate>
+            <AuthForm />
+        </AuthTemplate>
     )
 }
 
