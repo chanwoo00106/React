6321ca25f3fd796ea983fa57c1f418eea66b82aa
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-15 13:03:13 +0900

    로그인 폼 만들기

---
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// 회원가입 또는 로그인 폼을 보여줌
+
+const AuthFormBlock = styled.div``;
+
+const AuthForm = () => {
+    return (
+        <AuthFormBlock>
+            AuthForm
+        </AuthFormBlock>
+    )
+}
+
+export default AuthForm
---
+++ blog/blog-frontend/src/components/auth/AuthTemplate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// 회원가입/로그인 페이지 레이아웃
+
+const AuthTemplateBlock = styled.div``;
+
+const AuthTemplate = () => {
+    return (
+        <AuthTemplateBlock>
+            
+        </AuthTemplateBlock>
+    )
+}
+
+export default AuthTemplate
