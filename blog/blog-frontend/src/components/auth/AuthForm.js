8f1fa9fd8bd4a1515c59df8e94b4c242a5be07b7
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-15 23:09:28 +0900

    AuthForm 완성

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -1,14 +1,61 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+import Button from '../common/Button';
+
 
 // 회원가입 또는 로그인 폼을 보여줌
 
-const AuthFormBlock = styled.div``;
+const AuthFormBlock = styled.div`
+    h3 {
+        margin: 0;
+        color: ${palette.gray[8]};
+        margin-bottom: 1rem;
+    }
+`;
+
+const StyledInput = styled.input`
+    font-size: 1rem;
+    border: none;
+    border-bottom: 1px solid ${palette.gray[5]};
+    padding-bottom: 0.5rem;
+    outline: none;
+    width: 100%;
+    &:focus {
+        color: $oc-teal-7;
+        border-bottom: 1px solid ${palette.gray[7]};
+    }
+    & + & {
+        margin-top: 1rem;
+
+    }
+`;
+
+const Footer = styled.div`
+    margin-top: 2rem;
+    text-align: right;
+    a {
+        color: ${palette.gray[6]};
+        text-decoration: underline;
+    }
+    &:hover {
+        color: ${palette.gray[9]};
+    }
+`;
 
 const AuthForm = () => {
     return (
         <AuthFormBlock>
-            AuthForm
+            <h3>로그인</h3>
+            <form>
+                <StyledInput autoComplete="username" name="username" placeholder="아이디" />
+                <StyledInput autoComplete="new-password" name="password" placeholder="비밀번호" type="password" />
+                <Button>로그인</Button>
+            </form>
+            <Footer>
+                <Link to="/register">회원가입</Link>
+            </Footer>
         </AuthFormBlock>
     )
 }
