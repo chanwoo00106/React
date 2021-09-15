b42aed7b33726f5366b1c5ccfa9bf1d7d2987b51
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-15 12:10:35 +0900

    style

---
+++ blog/blog-frontend/src/components/common/Button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const StyleButton = styled.button`
+    border: none;
+    border-radius: 4px;
+    font-size: 1rem;
+    font-weight: bold;
+    padding: 0.25rem 1rem;
+    color: white;
+    outline: none;
+    cursor: pointer;
+    background: ${palette.gray[8]};
+    &:hover {
+        background: ${palette.gray[6]};
+    }
+`;
+
+const Button = props => <StyleButton {...props} />;
+
+export default Button;
--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Button from '../components/common/Button'
 
 const PostListPage = () => {
     return (
         <div>
-            포스트 리스트
+            <Button>버튼</Button>
         </div>
     )
 }
