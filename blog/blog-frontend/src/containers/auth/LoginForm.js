57d6d42e6b34b25673db1bd09315f6b62f78ca6b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-23 23:35:47 +0900

    로그인 상태 유지

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -54,6 +54,11 @@ const LoginForm = ({ history }) => {
     useEffect(() => {
         if (user) {
             history.push('/');
+            try {
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch (e) {
+                console.log('localStorage is not working');
+            }
         }
     }, [history, user]);
 
--- blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -67,6 +67,11 @@ const RegisterForm = ({ history }) => {
     useEffect(() => {
         if (user) {
             history.push('/');
+            try {
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch (e) {
+                console.log('localStorage is not working');
+            }
         }
     }, [user, history]);
 
