b3d59c02554edfa18a3edfd1a70813d7611ba492
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-29 23:22:02 +0900

    aside 구성

--- chanbook/src/App.js
+++ chanbook/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Header } from './components/Header';
+import { RightAside } from './components/RightAside';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <RightAside />
     </div>
   );
 }
---
+++ chanbook/src/components/RightAside.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { } from '@fortawesome/free-brands-svg-icons';
+import { faUserCircle, faUserFriends, faUsers, faTv, faHistory, faBookmark, faFlag, faCalendarCheck, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+export const RightAside = () => {
+    return (
+        <aside>
+            <ul>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faUserCircle} size="2x" />
+                        <p>변찬우</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faUserFriends} size="2x" />
+                        <p>친구</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faUsers} size="2x" />
+                        <p>그룹</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faTv} size="2x" />
+                        <p>Watch</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faHistory} size="2x" />
+                        <p>과거의 오늘</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faBookmark} size="2x" />
+                        <p>저장됨</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faFlag} size="2x" />
+                        <p>페이지</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faCalendarCheck} size="2x" />
+                        <p>이벤트</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#1">
+                        <FontAwesomeIcon icon={faChevronDown} size="2x" />
+                        <p>더보기</p>
+                    </a>
+                </li>
+            </ul>
+        </aside>
+    )
+}
