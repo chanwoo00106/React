6f5691aeeecfc1180677d647e82a422368f741f3
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-30 23:46:57 +0900

    add News.js

---
+++ chanbook/src/components/News.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const News = () => {
+    return (
+        <div>
+            <div className="user"></div>
+            <div className="text"></div>
+            <div className="emoji"></div>
+            <div className="comment"></div>
+        </div>
+    )
+}
+
+export default News
