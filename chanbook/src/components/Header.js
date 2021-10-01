01340580c3268858d676ce79d5f79375905aafb0
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-10-01 21:22:45 +0900

    Header styled component

--- chanbook/src/components/Header.js
+++ chanbook/src/components/Header.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { faFacebook, faReact, faFacebookMessenger } from '@fortawesome/free-brands-svg-icons';
 import { faHome, faUserFriends, faTv, faUsers, faUserCircle, faBars, faBell, faSortDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Header.css';
+import { HeaderS, HeaderLogo, SearchDiv, CenterUl, RightIcons } from './HeaderStyle';
 
 export const Header = () => {
     return (
-        <header>
-            <div className="header-logo">
+        <HeaderS>
+            <HeaderLogo>
                 <FontAwesomeIcon className="logo-icon" icon={faFacebook} size="3x" />
-                <div className="search-div"><input type="search" placeholder="   Facebook 검색" className="search" /></div>
-            </div>
+                <SearchDiv><input type="search" placeholder="   Facebook 검색" className="search" /></SearchDiv>
+            </HeaderLogo>
             <div>
-                <ul className="centerUl">
+                <CenterUl>
                     <li><a href="#1"><FontAwesomeIcon className="icons hover-icon" icon={faHome} size="2x" /></a></li>
                     <li><a href="#1"><FontAwesomeIcon className="icons" icon={faUserFriends} size="2x" /></a></li>
                     <li><a href="#1"><FontAwesomeIcon className="icons" icon={faTv} size="2x" /></a></li>
                     <li><a href="#1"><FontAwesomeIcon className="icons" icon={faUsers} size="2x" /></a></li>
                     <li><a href="#1"><FontAwesomeIcon className="icons" icon={faReact} size="2x" /></a></li>
-                </ul>
+                </CenterUl>
             </div>
-            <div className="right-icons">
+            <RightIcons>
                 <span className="user">
                     <span className="right-icon"><FontAwesomeIcon icon={faUserCircle} size="2x" /></span>
                     <span className="user-name">변찬우</span>
@@ -29,7 +29,7 @@ export const Header = () => {
                 <span className="right-icon"><FontAwesomeIcon icon={faFacebookMessenger} size="2x" /></span>
                 <span className="right-icon"><FontAwesomeIcon icon={faBell} size="2x" /></span>
                 <span className="right-icon"><FontAwesomeIcon icon={faSortDown} size="2x" /></span>
-            </div>
-        </header>
+            </RightIcons>
+        </HeaderS>
     )
 }
