a0ce7f3b6f8cad3367e2d49088bfbb25850f6d71
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-10-11 14:42:59 +0900

    아니 왜 안되는 거야

--- weather/src/App.js
+++ weather/src/App.js
@@ -2,40 +2,66 @@ import './App.css';
 import axios from 'axios';
 import WeatherCard from './WeatherCard';
 import { useEffect, useState } from 'react';
+import produce from 'immer';
 import dotenv from "dotenv";
 dotenv.config();
 
+const initialState = [
+  {
+    id: 1,
+    state: 'Gwangju',
+    data: null
+  },
+  {
+    id: 2,
+    state: 'Seoul',
+    data: null
+  }
+]
+
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setData(null);
+    setData(initialState);
     setError(null);
     setLoading(true);
-    const get = async () => {
+    async function get() {
       try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=Gwangju&appid=${process.env.REACT_APP_API_KEYAPI_KEY}`
-        );
-        setData(response.data);
+        data.forEach(async (d, i) => {
+          console.log(d.state)
+          await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${d.state}&appid=${process.env.REACT_APP_API_KEY}`
+          ).then(async response => {
+            console.log(response.data)
+            await setData(produce(data, draft => {
+              draft[i].data = response.data
+            }));
+            console.log(data);
+          })
+        })
       } catch (e) {
         setError(e);
       }
-      setLoading(false);
     }
     get();
+    setLoading(false);
   }, []);
 
+
   if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!data) return null;
+  else if (error) return <div>에러가 발생했습니다</div>;
+  else if (data[1].data === null || data[0].data === null) return null;
 
   else {
+    console.log(data)
     return (
       <div className="App">
-        <WeatherCard data={data} />
+        {data.map(i => (
+          <WeatherCard key={i.id} data={i.data} />
+        ))}
       </div>
     );
   }
