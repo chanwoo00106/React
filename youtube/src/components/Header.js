0b7071c01e991b3117533b61eed5ec53ce67b06b
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-10-14 23:44:50 +0900

    header 완성

--- youtube/src/components/Header.js
+++ youtube/src/components/Header.js
@@ -1,20 +1,47 @@
-import React from 'react'
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react'
+import { faBars, faKeyboard, faTimes, faVideo, faTh, faBell, faUserCircle, faMicrophone, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Wrapper } from './style/Header.styles';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
+    const [input, setInput] = useState();
+
+    const onChange = e =>
+        setInput(e.target.value);
+
+    const onClick = () =>
+        setInput("");
+
     return (
         <Wrapper>
             <h1>
-                <div className="logo-bars">
+                <div className="start">
                     <FontAwesomeIcon icon={faBars} size="sm" />
                     <Link to="/">
                        <img src="./YouTube.png" className="logo" alt="logo" />
                     </Link>
                 </div>
             </h1>
+            <form className="center">
+                <div className="input-wrap">
+                    <input placeholder="검색" value={input} onChange={onChange} />
+                    <FontAwesomeIcon icon={faKeyboard} size="1x" />
+                    {input && <FontAwesomeIcon className="remove" onClick={onClick} icon={faTimes} size="1x" />}
+                </div>
+                <button className="search-btn">
+                    <FontAwesomeIcon icon={faSearch} size="2x" />
+                </button>
+                <FontAwesomeIcon className="microphone" icon={faMicrophone} size="lg" />
+            </form>
+            <div className="end">
+                <div className="buttons">
+                    <FontAwesomeIcon icon={faVideo} size="lg" />
+                    <FontAwesomeIcon icon={faTh} size="lg" />
+                    <FontAwesomeIcon icon={faBell} size="lg" />
+                    <FontAwesomeIcon icon={faUserCircle} size="2x" />
+                </div>
+            </div>
         </Wrapper>
     )
 }
--- youtube/src/components/style/Header.styles.js
+++ youtube/src/components/style/Header.styles.js
@@ -1,9 +1,60 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    height: 10vh;
+    height: 6vh;
     padding: 0 20px;
-    .logo {
-        width: 100px;
+    display: flex;
+    justify-content: space-between;
+
+    .start {
+        a {
+            margin-left: 20px;
+
+            .logo {
+                width: 100px;
+            }
+        }
+    }
+    .center {
+        height: 100%;
+        width: 50%;
+        max-width: 640px;
+        display: flex;
+        align-items: center;
+
+        .input-wrap {
+            border: 1px solid #CCCCCC;
+            height: 70%;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            padding: 5px;
+            gap: 5px;
+
+    
+            input {
+                width: 100%;
+                height: 100%;
+                border: none;
+                outline: none;
+            }
+        }
+        .search-btn {
+            height: 70%;
+            padding: 0 20px;
+            border: 1px solid #CCCCCC;
+            border-left: none;
+        }
+        .microphone {
+            margin-left: 20px;
+        }
+    }
+
+    .buttons {
+        width: 200px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
     }
 `;
