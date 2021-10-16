c7a831a71ad0b787b4873b727625bd6fa6cf80cc
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-10-16 21:33:38 +0900

    side 완성

--- youtube/src/components/SideMenu.js
+++ youtube/src/components/SideMenu.js
@@ -2,73 +2,175 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Wrapper } from './style/SideMenu.styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faArchive, faHistory, faPhotoVideo, faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import { faCompass, faClock, faThumbsUp } from '@fortawesome/free-regular-svg-icons';
+import { faHome, faArchive, faHistory, faPhotoVideo, faChevronDown, faUserCircle, faFilm, faGamepad, faStream, faCog, faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { faCompass, faClock, faThumbsUp, faLightbulb, faFlag, faQuestionCircle, faCommentAlt } from '@fortawesome/free-regular-svg-icons';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 
 export const SideMenu = () => {
     return (
         <Wrapper>
-            <ul className="menu">
+            <ul>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faHome} size="lg" />
                         <p>홈</p>
                     </Link>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faCompass} size="lg" />
                         <p>탐색</p>
                     </Link>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faYoutube} size="lg" />
                         <p>구독</p>
                     </Link>
                 </li>
             </ul>
             <hr />
-            <ul className="menu">
+            <ul>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faArchive} size="lg" />
                         <p>보관함</p>
                     </Link>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faHistory} size="lg" />
                         <p>시청 기록</p>
                     </Link>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faPhotoVideo} size="lg" />
                         <p>내 동영상</p>
                     </Link>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faClock} size="lg" />
                         <p>나중에 볼 동영상</p>
                     </Link>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faThumbsUp} size="lg" />
                         <p>좋아요 표시한 동영상</p>
                     </Link>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/">
                         <FontAwesomeIcon icon={faChevronDown} size="lg" />
                         <p>더보기</p>
                     </Link>
                 </li>
             </ul>
+            <hr />
+            <p className="sideTitle">구독</p>
+            <ul>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faUserCircle} size="lg" />
+                        <p>수면재</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faUserCircle} size="lg" />
+                        <p>baekteun</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faUserCircle} size="lg" />
+                        <p>기타</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faUserCircle} size="lg" />
+                        <p>등등</p>
+                    </Link>
+                </li>
+            </ul>
+            <hr />
+            <p className="sideTitle"><b>YOUTUBE</b> 더보기</p>
+            <ul>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faYoutube} size="lg" />
+                        <p><b>YouTube Premium</b></p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faFilm} size="lg" />
+                        <p>영화</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faGamepad} size="lg" />
+                        <p>게임</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faStream} size="lg" />
+                        <p>실시간</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faLightbulb} size="lg" />
+                        <p>학습</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faTrophy} size="lg" />
+                        <p>스포츠</p>
+                    </Link>
+                </li>
+                <hr />
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faCog} size="lg" />
+                        <p>설정</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faQuestionCircle} size="lg" />
+                        <p>신고 기록</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faCommentAlt} size="lg" />
+                        <p>고객센터</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <FontAwesomeIcon icon={faFlag} size="lg" />
+                        <p>의견 보내기</p>
+                    </Link>
+                </li>
+            </ul>
+            <hr />
+            <p className="comment">
+                정보 보도자료 저작권 문의하기 크리에이터 광고 개발자<br />
+                약관 개인정보처리방침 정책 및 안전YouTube 작동의 원리 새로운 기능 테스트하기<br />
+                © 2021 Google LLC<br />
+                CEO: 선다 피차이<br />
+                주소: 1600 Amphitheatre Parkway, Mountain View, CA 94043, USA.<br />
+                전화: 080-822-1450(무료)
+            </p>
         </Wrapper>
     )
 }
--- youtube/src/components/style/SideMenu.styles.js
+++ youtube/src/components/style/SideMenu.styles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.aside`
-    width: 240px;
-    height: 90vh;
+    width: 250px;
+    height: 94vh;
     background-color: #ffffffff;
     padding-right: 20px;
     overflow: auto;
@@ -23,7 +23,33 @@ export const Wrapper = styled.aside`
     }
 
     hr {
-        color: #E5E5E5;
+        border: 1px solid #e5e5e5;
         margin: 10px 0;
+    }
+
+    .sideTitle {
+        width: 100%;
+        height: 40px;
+        padding: 10px 20px;
+    }
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        height: 56px;
+        border-radius: 8px;
+        background: #ffffffff;
+        width: 8px;
+    }
+    &:hover {
+        &::-webkit-scrollbar-thumb{
+            background-color: rgba(0,0,0,.1);
+        }
+    }
+
+    .comment {
+        padding: 20px;
+        color: #606060;
     }
 `;
