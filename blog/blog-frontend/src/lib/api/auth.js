59b7dc8599eb692aeaeb96d1b6f61852c581eb26
변찬우 <chan00106@naver.com>
2021-11-06 20:30:04 +0900

    restart frontend

--- blog/blog-frontend/src/lib/api/auth.js
+++
@@ -1,9 +0,0 @@
-import client from './client';
-
-export const login = ({ username, password }) =>
-  client.post('/api/auth/login', { username, password });
-
-export const register = ({ username, password }) =>
-  client.post('/api/auth/register', { username, password });
-
-export const check = () => client.get('/api/auth/check');
--- blog/blog-frontend/src/lib/api/client.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const client = axios.create({
-  baseURL: 'http://localhost:8000',
-});
-
-export default client;
