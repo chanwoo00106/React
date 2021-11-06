423e058c1598ae87cba5e9e4dd818b2180e8b30f
변찬우 <chan00106@naver.com>
2021-11-06 22:18:45 +0900

    redux 적용

--- blog/frontend-blog/src/pages/PostListPage.js
+++ blog/frontend-blog/src/pages/PostListPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from '../components/common/button';
 
 export default function PostListPage() {
   return (
     <div>
-      <h1>포스트 리스트</h1>
+      <Button>버튼</Button>
     </div>
   );
 }
