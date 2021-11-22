025502f489f8372ac5500e431c4f7e803fae8ae9
변찬우 <chan00106@naver.com>
2021-11-22 23:50:43 +0900

    대충 만들어 보기

--- web-editor/src/App.js
+++ web-editor/src/App.js
@@ -1,7 +1,58 @@
-import "./App.css";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
 
 function App() {
-  return <div className="App"></div>;
+  const modules = {
+    toolbar: [
+      //[{ 'font': [] }],
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    //'font',
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "align",
+    "color",
+    "background",
+  ];
+
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="App">
+      <ReactQuill
+        style={{ height: "600px" }}
+        theme="snow"
+        modules={modules}
+        formats={formats}
+        value={value || ""}
+        onChange={(content, delta, source, editor) =>
+          setValue(editor.getHTML())
+        }
+      />
+    </div>
+  );
 }
 
 export default App;
