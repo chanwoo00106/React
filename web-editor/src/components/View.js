766dd7f90fa85db3192735082d7f1fb5ff4297cd
변찬우 <chan00106@naver.com>
2021-12-01 19:42:42 +0900

    좀더 좋게 만들기

--- web-editor/src/components/View.js
+++ web-editor/src/components/View.js
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remark2rehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
+import React from "react";
+import { Viewer } from "@toast-ui/react-editor";
 
 function View({ data }) {
-  const [html, setHtml] = useState("");
-  useEffect(() => {
-    async function setUp() {
-      const result = unified()
-        .use(remarkParse)
-        .use(remark2rehype)
-        .use(rehypeStringify)
-        .processSync(data)
-        .toString();
-      console.log(result);
-      setHtml(result);
-    }
-    setUp();
-  });
-  return (
-    <div className="code" dangerouslySetInnerHTML={{ __html: html }}></div>
-  );
+  return <Viewer initialValue={data} />;
 }
 
 export default View;
