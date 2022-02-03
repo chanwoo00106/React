a335cdd0852cdf7077334af78cd7b00a9a46c61a
변찬우 <chan00106@naver.com>
2022-02-03 23:21:17 +0900

    Delete weather

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,23 +1,15 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { Route } from 'react-router-dom'
-import { Main } from './page/Main';
-import { Quest } from './page/Quest';
-import {Subscribe} from './page/Subscribe';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Main } from "./page/Main";
+import { Quest } from "./page/Quest";
+import { Subscribe } from "./page/Subscribe";
 
 function App() {
   return (
     <div>
-      
-      <Route path="/" exact component={Main}/>
+      <Route path="/" exact component={Main} />
       <Route path="/quest" component={Quest} />
       <Route path="/subscribe" component={Subscribe} />
-=======
-function App() {
-  return (
-    <div>
-
->>>>>>> Stashed changes
     </div>
   );
 }
