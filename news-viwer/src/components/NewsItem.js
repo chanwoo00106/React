1357ca348a7dfa51a2b403c67c3984e817f473dc
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-10 15:22:18 +0900

    Update code

--- news-viwer/src/components/NewsItem.js
+++ news-viwer/src/components/NewsItem.js
@@ -2,56 +2,60 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function NewsItem({ article }) {
+  const NewsItemBlock = styled.div`
+    display: flex;
+    .thumbnail {
+      margin-right: 1rem;
+      img {
+        display: block;
+        width: 160px;
+        height: 100px;
+        object-fit: cover;
+      }
+    }
+    .contents {
+      h2 {
+        margin: 0;
+        a {
+          color: black;
+          text-decoration: none;
 
-    const NewsItemBlock = styled.div`
-        display: flex;
-        .thumbnail {
-            margin-right: 1rem;
-            img {
-                display: block;
-                width: 160px;
-                height: 100px;
-                object-fit: cover;
-            }
+          :hover {
+            text-decoration: underline;
+          }
         }
-        .contents {
-            h2 {
-                margin: 0;
-                a {
-                    color: black;
-                }
-            }
-            p {
-                margin: 0;
-                line-height: 1.5;
-                margin-top: 0.5rem;
-                white-space: normal;
-            }
-        }
-        & {
-            margin-top: 3rem;
-        }
-    `;
+      }
+      p {
+        margin: 0;
+        line-height: 1.5;
+        margin-top: 0.5rem;
+        white-space: normal;
+      }
+    }
+    & {
+      margin-top: 3rem;
+    }
+  `;
 
-    const {title, description, url, urlToImage} = article;
+  const { title, description, url, urlToImage } = article;
 
-    return (
-        <NewsItemBlock>
-            {urlToImage && (
-                <div className="thumbnail">
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                        <img src={urlToImage} alt="thumbnail" />
-                    </a>
-                </div>
-            )}
-            <div className="contents">
-                <h2>
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                        {title}
-                    </a>
-                </h2>
-                <p>{description}</p>
-            </div>
-        </NewsItemBlock>
-    )
+  return (
+    <NewsItemBlock>
+      {urlToImage && (
+        <div className="thumbnail">
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <img src={urlToImage} alt="thumbnail" />
+          </a>
+        </div>
+      )}
+      <div className="contents">
+        <h2>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </NewsItemBlock>
+  );
 }
