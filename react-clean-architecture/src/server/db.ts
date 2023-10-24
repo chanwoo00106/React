d42f2ab71232d8cca2cc324ed1e8948b82255387
변찬우 <byun00106@gmail.com>
2023-10-24 17:51:14 +0900

    fix: reset t3 app

---
+++ react-clean-architecture/src/server/db.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+import { env } from "@src/env.mjs";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
