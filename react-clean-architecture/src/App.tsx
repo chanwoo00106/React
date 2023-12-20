46fbd029502537a3e697708f13db5d6391e65c59
변찬우 <byun00106@gmail.com>
2023-12-20 09:46:20 +0900

    feat: add react-query
    react query 추가

--- react-clean-architecture/src/App.tsx
+++ react-clean-architecture/src/App.tsx
@@ -1,9 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 import AuthSymbols from './domain/auth/AuthSymbols'
 import SigninUseCase from './domain/auth/useCase/SigninUseCase'
 import useInjection from './hooks/useInjection'
+import UserSymbols from './domain/user/UserSymbols'
+import FetchMyInfoUseCase from './domain/user/usecase/FetchMyInfoUseCase'
 
 function App() {
   const signinUseCase = useInjection<SigninUseCase>(AuthSymbols.SigninUseCase)
+  const fetchMyInfoUseCase = useInjection<FetchMyInfoUseCase>(
+    UserSymbols.FetchMyInfoUseCase,
+  )
 
   const onClick = () => {
     signinUseCase.execute({
@@ -12,6 +18,11 @@ function App() {
       conformPassword: '1234',
     })
   }
+
+  useQuery({
+    queryKey: ['fetchMyInfo'],
+    queryFn: () => fetchMyInfoUseCase.execute(),
+  })
 
   return (
     <div>
--- react-clean-architecture/src/main.tsx
+++ react-clean-architecture/src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { DIProvider } from './libs/DIContext.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
-import { DIProvider } from './libs/DIContext.tsx'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DIProvider>
-      <App />
-    </DIProvider>
+    <QueryClientProvider client={queryClient}>
+      <DIProvider>
+        <App />
+      </DIProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
