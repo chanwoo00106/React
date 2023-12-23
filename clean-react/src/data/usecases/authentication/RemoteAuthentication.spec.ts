47c360fe7016516abfab0d4ff94e766bd1eecba1
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-12-23 23:03:33 +0900

    feat: add mock url

--- clean-react/src/data/usecases/authentication/RemoteAuthentication.spec.ts
+++ clean-react/src/data/usecases/authentication/RemoteAuthentication.spec.ts
@@ -1,13 +1,14 @@
 import { describe, test, expect } from 'vitest'
 import { RemoteAuthentication } from './RemoteAuthentication'
 import MockHttpClientSpy from '@/data/test/MockHttpClientSpy'
+import { faker } from '@faker-js/faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: MockHttpClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new MockHttpClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
@@ -18,7 +19,7 @@ const makeSut = (url: string = 'any_url'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
