ccce54674e8a72f6298d8675ba091d660fb230f3
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2024-01-08 22:37:32 +0900

    feat: use singleton decorator

--- clean-react/src/data/usecases/authentication/RemoteAuthentication.ts
+++ clean-react/src/data/usecases/authentication/RemoteAuthentication.ts
@@ -2,7 +2,9 @@ import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 import { Authentication, AuthenticationParams } from '@/domain/usecases'
+import Singleton from '@/utils/Singleton'
 
+@Singleton
 export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
--- clean-react/src/infra/http/axiosHttpClient/AxiosHttpClient.ts
+++ clean-react/src/infra/http/axiosHttpClient/AxiosHttpClient.ts
@@ -4,7 +4,9 @@ import {
   HttpResponse,
 } from '@/data/protocols/http'
 import axios from 'axios'
+import Singleton from '@/utils/Singleton'
 
+@Singleton
 class AxiosHttpClient implements HttpPostClient<any, any> {
   async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
     const httpResponse = await axios.post(params.url, params.body)
